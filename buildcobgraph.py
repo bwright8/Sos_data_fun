600470278e16ec01f8768b5d2f579da9639ee266
bwright8 <60297325+bwright8@users.noreply.github.com>
2021-02-14 14:35:46 -0500

    Create buildcobgraph.py

---
+++ buildcobgraph.py
@@ -0,0 +1,33 @@
+"""
+PostgreSQL database related functions. 
+"""
+
+import os
+import sys
+from io import StringIO
+from logger import logger
+from dotenv import load_dotenv
+import pandas as pd
+import psycopg2
+from psycopg2 import sql
+import psycopg2.extras as extras
+import meta_data as md
+
+load_dotenv()
+local_dev = os.getenv("LOCAL_DEV") == "true"
+
+
+def get_database_connection(local_dev=True):
+    """Connection to PSQL DB."""
+    if local_dev:
+        conn = psycopg2.connect(os.getenv("LOCAL_DATABASE_URL"))
+    else:
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    return conn
+
+conn = get_database_connection(local_dev=local_dev)
+cursor = conn.cursor()
+
+cursor.execute("SELECT filing_num FROM master LIMIT 25;")
+qs = cursor.fetchall()
+print(type(qs))
